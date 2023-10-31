1571d69e1faa708172130741e048879705a29efb
Kim Soyeon <kim.soyeon@plus-zero.co.jp>
2023-10-31 16:57:43 +0900

    Update formatter.lua: Add ref URL

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,3 +1,4 @@
+-- https://github.com/tjtrabue/dotfiles/blob/414a1e277324ba6f8b8c37a22f0825abf32b3f02/link/config/nvim/plug-config/formatter.config.lua#L93
 local function formatter_sql_formatter()
   return {
     exe = "sql-formatter",
