bb1ace7c6ec856c90f13decb4c745c5b29787fb3
Kim Soyeon <kim.soyeon@plus-zero.co.jp>
2023-10-31 15:23:22 +0900

    Install nvim surround plugin

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -75,4 +75,11 @@ return {
       end,
     },
   },
+  {
+    -- https://github.com/kylechui/nvim-surround
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = true,
+  },
 }
