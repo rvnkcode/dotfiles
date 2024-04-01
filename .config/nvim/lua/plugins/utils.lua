7a3701180065710251b749c2887ccc4a40c1b8f6
rvnkcode <sykim.p508@gmail.com>
2024-04-01 13:45:10 +0900

    added a plugin: markdown preview

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -93,4 +93,13 @@ return {
     "github/copilot.vim",
     event = "VeryLazy",
   },
+  {
+    -- https://github.com/iamcco/markdown-preview.nvim
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
