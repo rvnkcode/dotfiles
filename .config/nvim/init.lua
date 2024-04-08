27f71224d22ec4f930410c5e40e15324373d5bba
rvnkcode <sykim.p508@gmail.com>
2024-04-08 18:30:43 +0900

    Updated `init.lua`: clipboard on the wayland configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -120,6 +120,23 @@ if is_wsl() then
     },
     cache_enabled = true,
   }
+elseif vim.env.WAYLAND_DISPLAY then
+  vim.g.clipboard = {
+    name = "WL-Clipboard with ^M Trim",
+    copy = {
+      ["+"] = "wl-copy --foreground --type text/plain",
+      ["*"] = "wl-copy --foreground --type text/plain --primary",
+    },
+    paste = {
+      ["+"] = function()
+        return vim.fn.systemlist('wl-paste --no-newline | tr -d "\r"', "", 1)
+      end,
+      ["*"] = function()
+        return vim.fn.systemlist('wl-paste --no-newline --primary | tr -d "\r"', "", 1)
+      end,
+    },
+    cache_enabled = 1,
+  }
 end
 
 -- ======================================================= Keymap
