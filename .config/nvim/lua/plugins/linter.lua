2970d52762be7ba8138a2c4372519e8b7830dcda
rvnkcode <sykim.p508@gmail.com>
2024-04-12 17:09:51 +0900

    Added a new linter: typos, codespell

--- .config/nvim/lua/plugins/linter.lua
+++ .config/nvim/lua/plugins/linter.lua
@@ -1,29 +1,29 @@
 -- ref:https://github.com/Srivatsan260/.dotfiles_v2/blob/29a728a10b63f62303114d5dbc0a0ce269f9b498/nvim/lua/plugins/linter.lua#L2
 return {
-  -- https://github.com/mfussenegger/nvim-lint
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+  {
+    -- https://github.com/mfussenegger/nvim-lint
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+      local linters = lint.linters
 
-    -- TODO: Install linter :Mason
-    lint.linters_by_ft = {
-      sql = { "sqlfluff" },
-      markdown = { "markdownlint" },
-    }
+      lint.linters_by_ft = {
+        sql = { "sqlfluff" },
+        markdown = { "markdownlint" },
+      }
 
-    local sqlfluff = lint.linters.sqlfluff
-    sqlfluff.args = { "lint", "--format=json", "--dialect=sqlite" }
+      linters.sqlfluff.args = { "lint", "--format=json", "--dialect=sqlite" }
+      linters.markdownlint.args = { "--disable", "MD013", "MD024", "MD026", "--" }
 
-    local markdownlint = lint.linters.markdownlint
-    markdownlint.args = { "--disable", "MD013", "MD024", "MD026", "--" }
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-      group = vim.api.nvim_create_augroup("LintGroup", { clear = true }),
-      pattern = { "*.sql", "*.md" },
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-  end,
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("LintGroup", { clear = true }),
+        callback = function()
+          lint.try_lint()
+          lint.try_lint("typos")
+          lint.try_lint("codespell")
+        end,
+      })
+    end,
+  },
 }
